d44fa565656afb4fbe07a101288cd8466d9ab920
Pisitbenz <5835512085@psu.ac.th>
2018-09-13 11:02:07 +0700

    cp2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
+
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
- }
+  render() {
+    return (
+      <div className="App">
+        <CharacterCard value="hello" />
+        <CharacterCard value="Pisit" />
+
+      </div>
+    );
+  }
 }
+
 export default App;
